1fe089388d8a10db5af9a640e1d4b79c5465feca
geekvinay <geekvinay002@gmail.com>
2023-05-17 13:53:37 +0530

    Deployed on render

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4320
+const port = process.env.PORT || 10000
 
 const cors = require('cors')
 app.use(cors())
